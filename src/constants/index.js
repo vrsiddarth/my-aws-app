d43c07ee697d19d0b06ecc3075a953be87601316
Siddarth V R <siddarthvr@knights.ucf.edu>
2020-08-11 21:10:36 -0700

    changing the request to https

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://erbsw-env.eba-72e4hwyb.us-west-2.elasticbeanstalk.com';
+export const API_BASE_URL = 'https://erbsw-env.eba-72e4hwyb.us-west-2.elasticbeanstalk.com';
 export const ACCESS_TOKEN = 'accessToken';
 export const OAUTH2_REDIRECT_URI = 'http://master.d18wgpc3oi5r8m.amplifyapp.com/oauth2/redirect'
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
