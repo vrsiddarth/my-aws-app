b186d17435c6307c5e6f808b1b49d9a25e0ad6d1
Siddarth V R <siddarthvr@knights.ucf.edu>
2020-08-11 21:21:02 -0700

    http to https

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL = 'https://erbsw-env.eba-72e4hwyb.us-west-2.elasticbeanstalk.com';
 export const ACCESS_TOKEN = 'accessToken';
-export const OAUTH2_REDIRECT_URI = 'http://master.d18wgpc3oi5r8m.amplifyapp.com/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'https://master.d18wgpc3oi5r8m.amplifyapp.com/oauth2/redirect'
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const GITHUB_AUTH_URL = API_BASE_URL + '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
