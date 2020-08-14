7238d0b4105fb5b1960c25d7a5ccad7fc66419f0
Siddarth V R <siddarthvr@knights.ucf.edu>
2020-08-13 23:57:02 -0700

    Changing it to https

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://erbsw-env.eba-72e4hwyb.us-west-2.elasticbeanstalk.com';
+export const API_BASE_URL = 'https://erbsw-env.eba-72e4hwyb.us-west-2.elasticbeanstalk.com';
 export const ACCESS_TOKEN = 'accessToken';
 export const OAUTH2_REDIRECT_URI = 'http://master.d18wgpc3oi5r8m.amplifyapp.com/oauth2/redirect'
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
@@ -6,7 +6,7 @@ export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redi
 export const GITHUB_AUTH_URL = API_BASE_URL + '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
 
 
-// export const API_BASE_URL = 'http://localhost:8080';
+// export const API_BASE_URL = 'https://localhost:8443';
 // export const ACCESS_TOKEN = 'accessToken';
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
 // export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
